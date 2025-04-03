9d11da86fa7d6578af9696dc688cb85fd2262fd5
MavisVermie <127209916+MavisVermie@users.noreply.github.com>
2025-04-04 00:34:59 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,15 +1,23 @@
+<?php
+// Start the session before ANY HTML is output
+session_start();
+include 'db.php';
+
+// Set up the database connection using mysqli
+$conn = mysqli_connect("localhost", "root", "", "helpingos");
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<title>HELPingos Website</title>
 	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-	<!-- Custom Theme files -->
 	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 	<link href="css/demo1.css" rel="stylesheet" type="text/css" media="all" />
-	<!-- Custom Theme files -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--webfont-->
 	<link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200,300,700' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
@@ -22,42 +30,43 @@
 		<div class="container">
 			<ul>
 				<?php 
-					session_start();
 					if(isset($_SESSION['user'])){
-						$user= $_SESSION['user'];
-						if($user=''){
-								echo "<li><i class='user'></i><a href='account.php'>Register/Log In</a></li>";
-							}
-							else{
-								echo "<li><i class='user'></i><a href='logout.php'>Log Out</a></li>";
-								echo "<li><i class='user'></i><a href='changepass.php'>Change Password</a></li>";
-							}
-						}
-						else{
+						$user = $_SESSION['user'];
+						if($user == ''){
 							echo "<li><i class='user'></i><a href='account.php'>Register/Log In</a></li>";
+						} else {
+							echo "<li><i class='user'></i><a href='logout.php'>Log Out</a></li>";
+							echo "<li><i class='user'></i><a href='changepass.php'>Change Password</a></li>";
 						}
+					} else {
+						echo "<li><i class='user'></i><a href='account.php'>Register/Log In</a></li>";
+					}
 				?>
 				
-				<li><i class="cart"></i><a href="cart.php">Cart<span><?php
-																			$conn=mysql_connect("localhost","root","");
-																				mysql_select_db("helpingos");
-																				$sql="SELECT count(*) FROM helpingos_cart";
-																				$retval=mysql_query($sql,$conn);
-																				while($count=mysql_fetch_array($retval)){
-																				$count=$count[0];
-																				echo "<span style='color:cream'>&nbsp;&nbsp;(".$count.")</span>";
-																			}
-																?></span>
-										</a>
+				<li><i class="cart"></i><a href="cart.php">Cart
+					<span>
+					<?php
+						$sql = "SELECT COUNT(*) FROM helpingos_cart";
+						$result = mysqli_query($conn, $sql);
+						if ($result) {
+							$row = mysqli_fetch_array($result);
+							$count = $row[0];
+							echo "<span style='color:cream'>&nbsp;&nbsp;(" . $count . ")</span>";
+						} else {
+							echo "<span style='color:red'>Error</span>";
+						}
+					?>
+					</span></a>
 				</li>
 			</ul>
 		</div>
 	</div>
+
 	<div class="header">
 		<div class="header-top">
 			<div class="container">
 				<div class="logo">
-				<br/>
+					<br/>
 					<a href="index.php"><img src="images/logo1.png" alt="" /></a>
 				</div>
 				<div class="top-menu">
@@ -74,6 +83,7 @@
 			</div>
 		</div>
 	</div>
-<!-- header-section-ends -->
-<!-- content-section-starts -->
+
+	<!-- header-section-ends -->
+	<!-- content-section-starts -->
 	<div class="content">
